69ed559796b3bb9a33604537155d432b1a35acbe
Pato2790 <pato.sartore@gmail.com>
2016-05-11 17:24:02 -0300

    Modificacion bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,11 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-	<link rel="stylesheet" href="../public/fonts/glyphicons-halflings-regular.eot">
+	<link rel="stylesheet" href="{{asset('fonts/glyphicons-halflings-regular.eot')}}">
 
     <!-- Styles -->
-    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
-	<link href="../public/css/app.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('css/app.css')}}" rel="stylesheet">
 
     <style>
         body {
@@ -86,12 +86,12 @@ glyphicon glyphicon-th-list"></i>Administrar</a></li>
 
     @yield('content')
 
-    <!-- JavaScripts -->
+    <!-- JavaScripts 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}} -->
 
-    <script type="text/javascript" src="{{ URL::asset('js/jquery-1.12.3.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery-1.12.3.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
