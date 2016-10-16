eff0b91d1b4adc77379bd2ba61bd80e13603e6f3
Pato2790 <pato.sartore@gmail.com>
2016-10-15 21:16:55 -0300

    Fix Login Face

--- app/FacebookAccount.php
+++ app/FacebookAccount.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FacebookAccount extends Model
 {
-    protected $fillable = ['user_id', 'email', 'provider_user_id', 'provider'];
+    protected $fillable = ['user_id', 'provider_user_id', 'provider'];
     public $timestamps = false;
 	protected $table = 'facebookaccount';
 
     public function user()
     {
-        return $this->belongsTo('App\User','id');
+        return $this->belongsTo('App\User','user_id');
     }
 }
